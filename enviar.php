336c07acdc9518c8f24bfedf9c1ba5cc1d5ed49b
Santiago Ocon <bryan_ocon512@hotmail.com>
2024-05-24 05:24:58 -0700

    first commit

---
+++ enviar.php
@@ -0,0 +1,11 @@
+<?php
+    $destino= "bryan_ocon512@hotmail.com"
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $telefono = $_POST["telefono"];
+    $mensaje = $_POST["mensaje"];
+    
+    $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+    mail($destino,"Contacto", $contenido);
+    header("Location:gracias.html");
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $servidor = "Localhost";
+    $usuario = "id21814372_bryan";
+    $clave = "Password5!";
+    $baseDeDatos = "id21814372_final";
+
+    $enlace = mysqli_connect ($servidor,$usuario, $clave, $baseDeDatos);
+
+?>
